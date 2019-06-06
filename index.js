301bcce2be57383637ffbaa9fd3acd68fc2dfa5b
Danny Tseng <github email address>
2019-06-06 08:55:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+
+function countdown(number) {
+  for (let i = 0; i <= number; i++) {
+    console.log(number - i);
+  }
+}
